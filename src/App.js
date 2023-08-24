4ab6032414a82daf8b96e4a6e14ecec31028658a
Vi Cruz <virmelcruz@gmail.com>
2023-08-25 01:52:46 +0800

    chore: POC useLocalStorage as websockets to the browser

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import { useSelector } from 'react-redux';
 import './App.css';
-import { Button } from '@mui/material';
+import { Button, TextField } from '@mui/material';
+import { useLocalStorage } from 'usehooks-ts';
 
-function App() {
+const App = () => {
   const user = useSelector((state) => state.user.user)
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useLocalStorage('messages', { list: [] });
+
+  const handleOnClick = () => {
+    setMessages({
+      list: [
+        ...messages.list, {
+        content: message,
+        type: 'mine'
+      }],
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,7 +27,22 @@ function App() {
         <p>
           {user.name}
         </p>
-        <Button variant="contained">Hello world</Button>
+        {messages.list.map(({ content, type }) => (
+          <div>
+            {content}
+            {type}
+          </div>
+        ))}
+        <TextField
+          id="filled-basic"
+          label="Filled"
+          variant="filled"
+          value={message}
+          onChange={(event) => {
+            setMessage(event.target.value);
+          }}
+        />
+        <Button variant="contained" onClick={handleOnClick} >Submit</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
